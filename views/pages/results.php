de258429532e81875c4dbf9af7ed5c99f860ab1d
Emmanuel Godwin <godwinemmanuel302@gmail.com>
2021-09-22 21:53:11 +0100

    Use Api for fetching data

--- views/pages/results.php
+++ views/pages/results.php
@@ -1,3 +1,12 @@
+<?php
+    $uri = 'http://api.football-data.org/v2/matches/';
+    $reqPrefs['http']['method'] = 'GET';
+    $reqPrefs['http']['header'] = 'X-Auth-Token: 305c37cae55d478789631eb31480e3a4';
+    $stream_context = stream_context_create($reqPrefs);
+    $response = file_get_contents($uri, false, $stream_context);
+    $result = json_decode($response, true);
+?>
+
 <?php $title = "Results | Soccer Predictor"; ?>
 
 <?php ob_start(); ?>
@@ -42,6 +51,16 @@
                         <?php
                     }
                 ?>
+
+                <?php foreach ($result['matches'] as $key => $value) { ?>
+                    <tr>
+                        <td><h3> &#x26BD;</h3></td>
+                        <td> <?= $value['status'] != "SCHEDULED" ? $value['status'] : $value['utcDate'] ?> </td>
+                        <td class="team_logo"> <?= empty($value['homeTeam']) ? "- vs -" : '<img src="'.'https://crests.football-data.org/'.$value['homeTeam']['id'].'.svg'.'" alt="">'.$value['homeTeam']['name'] ." vs " . '<img src="'.'https://crests.football-data.org/'.$value['awayTeam']['id'].'.svg'.'" alt="">'.$value['awayTeam']['name']  ?> </td>
+                        <td> <?= $value['score']['fullTime']['homeTeam'] ." - ". $value['score']['fullTime']['homeTeam'] ?> </td>
+                        <td> <?= $value['score']['fullTime']['homeTeam'] ." - ". $value['score']['fullTime']['homeTeam'] ?> </td>
+                    </tr>        
+                <?php } ?>
             </tbody>
         </table>
     </div>
