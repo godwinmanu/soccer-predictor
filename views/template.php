7f334cfdb23bec662baf9a4b2bd93e02b452518d
Emmanuel Godwin <godwinemmanuel302@gmail.com>
2021-09-23 10:58:38 +0100

    Fix links typo for hosting

--- views/template.php
+++ views/template.php
@@ -4,17 +4,17 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="/soccer_predictor/public/js/jquery-3.6.0.js"></script>
-    <script src="/soccer_predictor/public/js/bootstrap.min.js"></script>
-    <script src="/soccer_predictor/public/DataTables/js/jquery.dataTables.min.js"></script>
-    <script src="/soccer_predictor/public/DataTables/js/dataTables.bootstrap5.min.js"></script>
-    <link rel="stylesheet" href="/soccer_predictor/public/css/bootstrap.min.css">
-    <link rel="stylesheet" href="/soccer_predictor/public/DataTables/css/dataTables.bootstrap5.min.css">
-    <link rel="stylesheet" href="/soccer_predictor/public/Font_awesome/css/all.min.css">
-    <link rel="stylesheet" href="/soccer_predictor/public/css/main.css">
-    <link rel="stylesheet" href="/soccer_predictor/public/css/results.css">
-    <link rel="stylesheet" href="/soccer_predictor/public/css/contactus.css">
-    <link rel="stylesheet" href="/soccer_predictor/public/css/faq.css">
+    <script src="/public/js/jquery-3.6.0.js"></script>
+    <script src="/public/js/bootstrap.min.js"></script>
+    <script src="/public/DataTables/js/jquery.dataTables.min.js"></script>
+    <script src="/public/DataTables/js/dataTables.bootstrap5.min.js"></script>
+    <link rel="stylesheet" href="/public/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/public/DataTables/css/dataTables.bootstrap5.min.css">
+    <link rel="stylesheet" href="/public/Font_awesome/css/all.min.css">
+    <link rel="stylesheet" href="/public/css/main.css">
+    <link rel="stylesheet" href="/public/css/results.css">
+    <link rel="stylesheet" href="/public/css/contactus.css">
+    <link rel="stylesheet" href="/public/css/faq.css">
     <title> <?= $title ?> </title>
 </head>
 <body>
