ccf1f9e4853413120f8883e0e8119735b0986fcf
Emmanuel Godwin <godwinemmanuel302@gmail.com>
2021-09-26 11:33:48 +0100

    Review links path for pushing

--- views/template.php
+++ views/template.php
@@ -4,12 +4,16 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
 <!--     <script src="/public/js/jquery-3.6.0.js"></script>
     <script src="/public/js/bootstrap.min.js"></script>
     <script src="/public/DataTables/js/jquery.dataTables.min.js"></script>
     <script src="/public/DataTables/js/dataTables.bootstrap5.min.js"></script>
+    <script src="/public/DataTables/js/dataTables.responsive.min.js"></script>
+    <script src="/public/DataTables/js/responsive.bootstrap5.min.js"></script>
     <link rel="stylesheet" href="/public/css/bootstrap.min.css">
     <link rel="stylesheet" href="/public/DataTables/css/dataTables.bootstrap5.min.css">
+    <link rel="stylesheet" href="/public/DataTables/css/responsive.bootstrap5.min.css">
     <link rel="stylesheet" href="/public/Font_awesome/css/all.min.css">
     <link rel="stylesheet" href="/public/css/main.css">
     <link rel="stylesheet" href="/public/css/results.css">
